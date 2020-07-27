05f1d8be60d8dc211aae9037d61ab2ef6aa2ca5e
dvoso <dkvoso@gmail.com>
2020-07-27 13:00:02 +0000

    advanced template

--- app.py
+++ app.py
@@ -4,43 +4,34 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask_pymongo import PyMongo
-
+import os
+from dotenv import load_dotenv
 
 # -- Initialization section --
 app = Flask(__name__)
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+# first let's load environment variables in .env
+# load_dotenv()
+
+# then store environment variables with new names 
+# MONGO_USER = os.getenv("MONGO_USER")
+# MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 
 # name of database
-app.config['MONGO_DBNAME'] = 'database-name'
+# app.config['MONGO_DBNAME'] = 'test'
 
-# URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
+# URI of database - replace with your information 
+# app.config['MONGO_URI'] = f'mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@cluster0-7bt79.mongodb.net/test?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
 
 @app.route('/')
 @app.route('/index')
-
 def index():
+    # collection = mongo.db.events
+    # events = collection.find({})
     return render_template('index.html', events = events)
-
-
-# CONNECT TO DB, ADD DATA
-
-@app.route('/add')
-
-def add():
-    # connect to the database
-
-    # insert new data
-
-    # return a message to the user
-    return ""
